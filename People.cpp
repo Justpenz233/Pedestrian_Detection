1f0968af21db9dd16696a102285f538aa6f04f4f
Justpenz233 <j2152662@126.com>
2020-01-28 16:43:43 +0800

    Finish Detection

---
+++ People.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 李斯祺 on 2020/1/28.
+//
+
+#include "People.h"
---
+++ People.h
@@ -0,0 +1,14 @@
+//
+// Created by 李斯祺 on 2020/1/28.
+//
+
+#ifndef PRESIDENT_DETECTION_PEOPLE_H
+#define PRESIDENT_DETECTION_PEOPLE_H
+
+
+class People {
+
+};
+
+
+#endif //PRESIDENT_DETECTION_PEOPLE_H
