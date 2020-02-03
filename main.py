5dd80084c2f0658acc2f31d0434fdda95d5570e5
Justpenz233 <j2152662@126.com>
2020-02-04 00:08:40 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import cv2 as cv
-import numpy
-import colorsys
-import collections
 
-toleranceRange = 50
-toleranceCount = 30
+toleranceRange = 200
+toleranceCount = 5
+
+
+def calc_area(rect1, rect2):
+    xl1, yb1, xr1, yt1 = rect1
+    xl2, yb2, xr2, yt2 = rect2
+    xr1 += xl1
+    yt1 += yb1
+    xr2 += xl2
+    yt2 += yb2
+    xmin = max(xl1, xl2)
+    ymin = max(yb1, yb2)
+    xmax = min(xr1, xr2)
+    ymax = min(yt1, yt2)
+    width = xmax - xmin
+    height = ymax - ymin
+    if width <= 0 or height <= 0:
+        return 0
+    cross_square = width * height
+    return cross_square
+
+
+def toList(R: tuple):
+    result = []
+    for i in R:
+        result.append(i)
+    return result
+
+
+def RectContin(r1, r2):
+    r1 = toList(r1)
+    r2 = toList(r2)
+
+    R1Top = r1[0] + r1[2]
+    R1Right = r1[1] + r1[3]
+
+    R2Top = r2[0] + r2[2]
+    R2Right = r2[1] + r2[3]
+
+    if not R1Top > R2Top:
+        return False
 
 
 def CalcCenter(Rect):
@@ -13,7 +50,7 @@ def CalcCenter(Rect):
     :type Rect: tuple
     """
     x = int(Rect[0] + Rect[2] * 0.5)
-    y = int(Rect[2] + Rect[3] * 0.5)
+    y = int(Rect[1] + Rect[3] * 0.5)
     return (x, y)
 
 
@@ -44,7 +81,7 @@ class People:
 
 
 def mk_path(FileName):
-    return "/Users/makik/Desktop/FinalProject/" + FileName
+    return "/Users/makik/Desktop/untitled1/" + FileName
 
 
 def NextSecond(cap, Second, FPS):
@@ -63,7 +100,6 @@ if __name__ == '__main__':
     Height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
     Width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 
-
     SETWidth = 600
     Pedestrian = []
     while True:
@@ -71,10 +107,14 @@ if __name__ == '__main__':
         # frame = cv.resize(frame, (SETWidth, int(SETWidth * Height / Width)))
         if not isOpen:
             break
-        NextSecond(cap, 0.2, FPS)
+        NextSecond(cap, 0.05, FPS)
         rects, wei = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
 
-        rects = rects.tolist()
+        tR = []
+        for i in rects:
+            tR.append(i)
+        rects = tR
+
         for tP in Pedestrian:
             track_ok, bbox = tP.tracker.update(frame)
             if not track_ok:
@@ -84,12 +124,12 @@ if __name__ == '__main__':
             else:
                 tP.setPos(bbox)
 
-        for dC in rects:
-            for tP in Pedestrian:
-                pos = tP.Center
+        for tP in Pedestrian:
+            pos = tP.Center
+            for index, dC in enumerate(rects):
                 if dC[0] - toleranceRange < pos[0] < dC[0] + dC[2] + toleranceRange \
                         and dC[1] - toleranceRange < pos[1] < dC[1] + dC[3] + toleranceRange:
-                    rects.remove(dC)
+                    rects.pop(index)
 
         for R in rects:
             tR = tuple(R)
@@ -97,11 +137,16 @@ if __name__ == '__main__':
 
         for tP in Pedestrian:
             (x, y, w, h) = tP.Rect
+            x = int(x)
+            y = int(y)
+            w = int(w)
+            h = int(h)
             cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv.circle(frame, tP.Center, 3, (0, 0, 0), -1)
 
         # for (x, y, w, h) in rects:
         #     cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         cv.imshow("FRAME", frame)
-        if cv.waitKey(1) & 0xff == 27:  # esc键
+        if cv.waitKey(1) & 0xff == 27:
             break
